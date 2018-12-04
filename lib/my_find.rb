baaa2fade339f2ae1967b888fc6b4c463a7c5183
Wendi Chen <epanistamai.proles@gmail.com>
2018-12-04 18:17:24 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,10 @@
-require 'pry'
+
 
 def my_find(collection)
-
+    count = 0
+      for num in collection
+        if yield(collection[num])
+          return collection[num]
+        end
+      end
 end
